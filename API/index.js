0c8386f0a055461b3d6b01ebcce05979e6ed8bdc
Andrew <ghobrial.andrew@gmail.com>
2018-10-29 14:48:30 -0400

    api setup of server (index.js)

---
+++ API/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+server.use(helmet());
+server.use(express.json());
+
+const port = 5000;
+server.listen(port, () => console.log(`***API running on ${port}***`)); 
