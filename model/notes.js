27a25c778ba2073b1d21e36059c1ed95cbf7a87a
Andrew <ghobrial.andrew@gmail.com>
2018-10-29 16:26:50 -0400

    completed notes DB model

---
+++ model/notes.js
@@ -0,0 +1,29 @@
+const knex = require('knex');
+const knexConfig = require('../knexfile.js');
+const db = knex(knexConfig.development);
+ 
+module.exports = {
+    find,
+    findById,
+    add,
+    update,
+ };
+ 
+ function find() {
+    return db('notes');
+}
+ function findById(id) {
+    return db('notes')
+        .where({ id })
+        .first();
+}
+ function add(note) {
+    return db('notes')
+        .insert(note)
+        .into('notes');
+}
+ function update(id, edit) {
+    return db('notes')
+        .where({ id })
+        .update(edit);
+} 
